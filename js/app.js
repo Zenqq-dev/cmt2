b886c5dd5c4aa118dcb28bd837c3b3e7a1b35c5c
Zenqq-dev <79142916+Zenqq-dev@users.noreply.github.com>
2021-12-16 11:42:39 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
+console.log('я работаю')
 // Dynamic Adapt v.1
 // HTML data-da="where(uniq class name),when(breakpoint),position(digi)"
 // e.x. data-da=".item,992,2"
